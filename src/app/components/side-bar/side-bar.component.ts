7097596fb23cdbf9a11d2b76f6687df0f516116a
Kubus <jakub.kangowski7@vp.pl>
2024-08-16 12:55:55 +0200

    version 0.01 > initial commit

---
+++ src/app/components/side-bar/side-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'side-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-bar.component.html',
+  styleUrl: './side-bar.component.css'
+})
+export class SideBarComponent {
+
+}
