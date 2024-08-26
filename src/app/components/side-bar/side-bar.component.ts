4abd8c824a27cb5c74b104eb2f61eac826672271
Kubus <jakub.kangowski7@vp.pl>
2024-08-26 14:07:07 +0200

    version 0.02 > sidebar works alright, but needs tweaking

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import {NgClass, NgIf} from "@angular/common";
 
 @Component({
   selector: 'side-bar',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass,
+    NgIf
+  ],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrls: ['./side-bar.component.css']
 })
 export class SideBarComponent {
+  protected isSideBarBeingDisplayed: boolean = true;
+
+  protected toggleSideBar() {
+    this.isSideBarBeingDisplayed = !this.isSideBarBeingDisplayed;
+  }
+
+  protected toggleLanguageSelection(){}
 
 }
