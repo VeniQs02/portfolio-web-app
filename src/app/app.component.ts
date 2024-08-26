f72c2cc9b7bf3bd05f4526718f0b038d81f313f9
Kubus <jakub.kangowski7@vp.pl>
2024-08-26 14:55:35 +0200

    version 0.03 > sidebar incorporated with main section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {SideBarComponent} from "./components/side-bar/side-bar.component";
+import {MainSectionComponent} from "./components/main-section/main-section.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SideBarComponent],
+  imports: [RouterOutlet, SideBarComponent, MainSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
